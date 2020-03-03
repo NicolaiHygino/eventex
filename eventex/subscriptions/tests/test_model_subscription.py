fea2b38d9f362ae75fb07f3aab010102bb6d9a92
Nicolai Hygino <nicolaihygino2000@gmail.com>
2020-03-03 08:56:45 -0300

    Adiciona campo Paid

--- eventex/subscriptions/tests/test_model_subscription.py
+++ eventex/subscriptions/tests/test_model_subscription.py
@@ -26,3 +26,7 @@ class SubscriptionModelTest(TestCase):
 
 	def test_str(self):
 		self.assertEqual('Nicolai Hygino', str(self.obj))
+
+	def test_paid_default_to_False(self):
+		"""By default paid most be False."""
+		self.assertEqual(False, self.obj.paid)
